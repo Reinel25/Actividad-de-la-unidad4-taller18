222a467de4d19ca7d328d7d6447c03540c5c101d
Reinel <rcaballeron@unicartagena.edu.co>
2025-11-07 13:06:58 -0500

    Ejercicio 3 terminado

---
+++ src/Ejercicio3/Automovil.java
@@ -0,0 +1,8 @@
+package Ejercicio3;
+
+public class Automovil implements Conducir {
+    @Override
+    public void conducir() {
+        System.out.println("El automóvil está en marcha.");
+    }
+}
---
+++ src/Ejercicio3/Camion.java
@@ -0,0 +1,8 @@
+package Ejercicio3;
+
+public class Camion implements Transporte {
+    @Override
+    public void cargarMercancias() {
+        System.out.println("El camión está cargando mercancías.");
+    }
+}
