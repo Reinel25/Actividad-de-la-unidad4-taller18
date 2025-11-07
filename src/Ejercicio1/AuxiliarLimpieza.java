5e25ba95772d5f8fe9f065f72fca8ded2f68bd8a
Reinel <rcaballeron@unicartagena.edu.co>
2025-11-07 11:04:28 -0500

    Ejercicio 1 terminado

---
+++ src/Ejercicio1/AuxiliarLimpieza.java
@@ -0,0 +1,8 @@
+package Ejercicio1;
+
+public class AuxiliarLimpieza implements Limpieza {
+    @Override
+    public void limpiar() {
+        System.out.println("El auxiliar está limpiando las instalaciones.");
+    }
+}
