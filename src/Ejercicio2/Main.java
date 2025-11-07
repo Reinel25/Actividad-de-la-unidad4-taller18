367b8c6ad8e1cf6273c13c7acaca19dd0098c9a5
Reinel <rcaballeron@unicartagena.edu.co>
2025-11-07 11:48:26 -0500

    correccion del main

--- src/Ejercicio2/Main.java
+++ src/Ejercicio2/Main.java
@@ -3,11 +3,11 @@ package Ejercicio2;
 public class Main {
     public static void main(String[] args) {
 
-        //Transferencia transferencia = new ServicioTransferencia();
+        Transferencia transferencia = new ServicioTransferencia();
         Retiro retiro = new ServicioRetiro();
         PagoFactura pago = new ServicioPagoFactura();
 
-        //transferencia.transferir(500);
+        transferencia.transferir(500);
         retiro.retirar(200);
         pago.pagarFactura("Factura 1234");
     }
